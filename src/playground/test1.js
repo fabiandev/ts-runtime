9577d2da2de5b46cba6a78014de7ead3ff880b71
Fabian Pirklbauer <fabian.pirklbauer@gmail.com>
2017-01-30 21:39:50 +0100

    Testing.

--- src/playground/test1.js
+++ src/playground/test1.js
@@ -1,8 +1,12 @@
 function test(param1: string, param2?: boolean[]): [number, string] {
     let _param1Type = t.string(), _param2Type = t.array(t.boolean());
+    const _returnType = t.return(t.tuple(t.number(), t.string()));
     t.param("param1", _param1Type, false).assert(param1);
     t.param("param2", _param2Type, true).assert(param2);
+    let _anotherType = t.number(), another;
     const num = t.number().assert(10);
     let _strType = t.string(), str = _strType.assert('hi');
+    let x = 10;
     return [num, str];
 }
+t.annotate(test, t.function(t.param("param1", t.string()), t.param("param2", t.array(t.boolean())), t.return(t.tuple(t.number(), t.string()))));
--- src/playground/test1.ts
+++ src/playground/test1.ts
@@ -1,5 +1,7 @@
 function test(param1: string, param2?: boolean[]): [number, string] {
+  let another: number;
   const num: number = 10;
   let str: string = 'hi';
+  let x = 10;
   return [num, str];
 }
