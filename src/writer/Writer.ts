2fa858990879b97c686da377817145c795be15ce
Fabian Pirklbauer <fabian.pirklbauer@gmail.com>
2017-01-30 21:40:30 +0100

    Minor changes on how the debug writer accesses configurations.

--- src/writer/Writer.ts
+++ src/writer/Writer.ts
@@ -1,7 +1,6 @@
 import * as path from 'path';
 import * as fs from 'fs';
 import { CompilerResult, FileResult } from '../compiler';
-import Options from '../options/Options';
 import WriterConfig from './WriterConfig';
 import WriterResult from './WriterResult';
 import DEFAULT_CONFIG from './default_config';
@@ -24,17 +23,17 @@ export class Writer {
 
   public writeFile(fileResult: FileResult, config?: WriterConfig): Promise<WriterResult> {
     return new Promise((resolve, reject) => {
-      const options: WriterConfig = config ? Object.assign({}, DEFAULT_CONFIG, config) : DEFAULT_CONFIG;
-      const file = fileResult.filePath.replace(new RegExp(`^${options.basePath}`), '');
-      let location = path.join(options.writePath as string, file);
+      config = config ? Object.assign({}, DEFAULT_CONFIG, config) : DEFAULT_CONFIG;
+      const file = fileResult.filePath.replace(new RegExp(`^${config.basePath}`), '');
+      let location = path.join(config.writePath as string, file);
 
       location = path.join(
-        path.dirname(path.join(options.writePath as string, file)),
+        path.dirname(path.join(config.writePath as string, file)),
         `${path.basename(location, '.ts')}.js`,
       );
 
       fs.writeFile(location, fileResult.result, {
-        encoding: options.encoding,
+        encoding: config.encoding,
       }, err => {
         if (err) {
           return reject(`Error writing ${location}`);
