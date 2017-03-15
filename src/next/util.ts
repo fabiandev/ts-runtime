e28cd0819b9723f5420d428f2e0c1449bd5e5fec
Fabian Pirklbauer <fabian.pirklbauer@gmail.com>
2017-03-15 22:47:43 +0100

    Util fixes and additions.

--- src/next/util.ts
+++ src/next/util.ts
@@ -13,14 +13,29 @@ export function getScope(node: ts.Node): ts.Node {
     return node;
   }
 
-  if (this.isScope(node.kind)) {
+  if (isScopeKind(node.kind)) {
     return node;
   }
 
-  return this.getScope(node.parent);
+  return getScope(node.parent);
 }
 
-export function isScope(kind: ts.SyntaxKind): boolean {
+// TODO: test
+export function identifierExistsUp(node: ts.Node, id: string | ts.Identifier) {
+  if (typeof id !== 'string') id = id.text;
+  let current = node;
+  while (current.parent) current = current.parent;
+  return identifierExistsDown(current, id, node);
+}
+
+// TODO: test
+export function identifierExistsDown(node: ts.Node, id: string, stop?: ts.Node): boolean {
+  if (!node || node === stop) return false;
+  return node.kind === ts.SyntaxKind.Identifier && (node as ts.Identifier).text === id ?
+    true : !!ts.forEachChild(node, n => identifierExistsDown(n, id, stop));
+}
+
+export function isScopeKind(kind: ts.SyntaxKind): boolean {
   switch (kind) {
     case ts.SyntaxKind.SourceFile:
     case ts.SyntaxKind.Block:
