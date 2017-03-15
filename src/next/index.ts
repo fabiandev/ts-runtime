4c39f47a4135eee16dd803f7d0e522368070d928
Fabian Pirklbauer <fabian.pirklbauer@gmail.com>
2017-03-15 21:38:44 +0100

    Package exports.

--- src/next/index.ts
+++ src/next/index.ts
@@ -1,4 +1,9 @@
-export * from './bus';
+import * as b from './bus';
+export const bus = b;
+
+import * as u from './util';
+export const util = u;
+
 export * from './context';
 export * from './mutators';
 export * from './options';
