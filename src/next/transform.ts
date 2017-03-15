4c646aa49f9d2503bb735745fdcb633ce3d5956a
Fabian Pirklbauer <fabian.pirklbauer@gmail.com>
2017-03-15 21:38:02 +0100

    Restore parent nodes. Determine correct scope. Other improvements and fixes.

--- src/next/transform.ts
+++ src/next/transform.ts
@@ -1,6 +1,7 @@
 import * as path from 'path';
 import * as rimraf from 'rimraf';
 import * as ts from 'typescript';
+import * as util from './util';
 import * as bus from './bus';
 import { MutationContext } from './context';
 import { mutators } from './mutators';
@@ -10,7 +11,7 @@ const DEBUG = true;
 
 export function transform(entryFile: string, options?: Options): void {
   options = getOptions(options);
-  bus.emitter.emit(bus.events.START, { entryFile, options });
+  notify(bus.events.START, { entryFile, options });
 
   const basePath = path.dirname(entryFile);
   const host = ts.createCompilerHost(options.compilerOptions);
@@ -27,19 +28,19 @@ export function transform(entryFile: string, options?: Options): void {
   }
 
   if (!check(diagnostics)) {
-    bus.emitter.emit(bus.events.DIAGNOSTICS_TRANSFORM, { diagnostics });
-    bus.emitter.emit(bus.events.DIAGNOSTICS_TRANSFORM_PRE, { diagnostics });
+    notify(bus.events.DIAGNOSTICS_TRANSFORM, { diagnostics });
+    notify(bus.events.DIAGNOSTICS_TRANSFORM_PRE, { diagnostics });
   }
 
   const sourceFiles = program.getSourceFiles().filter(sf => !sf.isDeclarationFile);
 
-  bus.emitter.emit(bus.events.TRANSFORM_START, { sourceFiles });
+  notify(bus.events.TRANSFORM_START, { sourceFiles });
   const result = ts.transform(sourceFiles, [transformer], options.compilerOptions);
-  bus.emitter.emit(bus.events.TRANSFORM_END, { sourceFiles: result.transformed });
+  notify(bus.events.TRANSFORM_END, { sourceFiles: result.transformed });
 
   if (!check(result.diagnostics)) {
-    bus.emitter.emit(bus.events.DIAGNOSTICS_TRANSFORM, { diagnostics });
-    bus.emitter.emit(bus.events.DIAGNOSTICS_TRANSFORM_POST, { diagnostics: result.diagnostics });
+    notify(bus.events.DIAGNOSTICS_TRANSFORM, { diagnostics });
+    notify(bus.events.DIAGNOSTICS_TRANSFORM_POST, { diagnostics: result.diagnostics });
   }
 
   const printerOptions: ts.PrinterOptions = {
@@ -53,7 +54,7 @@ export function transform(entryFile: string, options?: Options): void {
     }
   };
 
-  bus.emitter.emit(bus.events.WRITE_START, { count: result.transformed.length });
+  notify(bus.events.WRITE_START, { count: result.transformed.length });
   const printer = ts.createPrinter(printerOptions, printHandlers);
 
   let count = 0;
@@ -61,29 +62,30 @@ export function transform(entryFile: string, options?: Options): void {
     count++;
     const location = toTempFilePath(transformed.fileName, basePath, options.tempFolderName);
     const source = printer.printFile(transformed);
-    bus.emitter.emit(bus.events.WRITE_FILE_START, { location, transformed, source });
+    notify(bus.events.WRITE_FILE_START, { location, transformed, source });
     ts.sys.writeFile(location, source);
-    bus.emitter.emit(bus.events.WRITE_FILE_END, { location, transformed, source });
+    notify(bus.events.WRITE_FILE_END, { location, transformed, source });
   }
 
-  bus.emitter.emit(bus.events.WRITE_END, { count });
+  notify(bus.events.WRITE_END, { count });
+
   result.dispose();
 
   emit(entryFile, options);
 
   if (!options.keepTempFiles) {
     const tempPath = getTempPath(basePath, options.tempFolderName);
-    bus.emitter.emit(bus.events.TEMP_DELETE_START, { tempPath });
+    notify(bus.events.TEMP_DELETE_START, { tempPath });
     rimraf.sync(getTempPath(basePath, options.tempFolderName));
-    bus.emitter.emit(bus.events.TEMP_DELETE_END, { tempPath });
+    notify(bus.events.TEMP_DELETE_END, { tempPath });
   }
 
-  bus.emitter.emit(bus.events.END, { entryFile, options });
+  notify(bus.events.END, { entryFile, options });
 }
 
 export function emit(entryFile: string, options: Options) {
   const tempEntryFile = toTempFilePath(entryFile, path.dirname(entryFile), options.tempFolderName);
-  bus.emitter.emit(bus.events.EMIT_START, { entryFile, tempEntryFile, options });
+  notify(bus.events.EMIT_START, { entryFile, tempEntryFile, options });
 
   options.compilerOptions.outDir = path.dirname(entryFile);
 
@@ -102,31 +104,33 @@ export function emit(entryFile: string, options: Options) {
 
 
   if (!check(diagnostics)) {
-    bus.emitter.emit(bus.events.DIAGNOSTICS_EMIT, { diagnostics });
-    bus.emitter.emit(bus.events.DIAGNOSTICS_EMIT_PRE, { diagnostics });
+    notify(bus.events.DIAGNOSTICS_EMIT, { diagnostics });
+    notify(bus.events.DIAGNOSTICS_EMIT_PRE, { diagnostics });
   }
 
   const emitResult = program.emit();
 
   if (!check(emitResult.diagnostics)) {
-    bus.emitter.emit(bus.events.DIAGNOSTICS_EMIT, { diagnostics: emitResult.diagnostics });
-    bus.emitter.emit(bus.events.DIAGNOSTICS_EMIT_POST, { diagnostics: emitResult.diagnostics });
+    notify(bus.events.DIAGNOSTICS_EMIT, { diagnostics: emitResult.diagnostics });
+    notify(bus.events.DIAGNOSTICS_EMIT_POST, { diagnostics: emitResult.diagnostics });
   }
 
-  bus.emitter.emit(bus.events.EMIT_END, { entryFile, tempEntryFile, options });
+  notify(bus.events.EMIT_END, { entryFile, tempEntryFile, options });
 }
 
 function onBeforeVisit(node: ts.Node, mc: MutationContext, tc: ts.TransformationContext): ts.Node {
+  if (util.isKind(node, ts.SyntaxKind.SourceFile)) {
+    mc.setSourceFile(node);
+  }
+
   mc.setTransformationContext(tc);
-  mc.setSourceFile(node);
-  mc.setScope(node);
+  mc.setScope(util.getScope(node));
 
   return node;
 }
 
 function onAfterVisit(node: ts.Node, mc: MutationContext, tc: ts.TransformationContext): ts.Node {
   mc.addVisited(node);
-
   return node;
 }
 
@@ -134,24 +138,40 @@ function transformer(context: ts.TransformationContext): ts.Transformer<ts.Sourc
   const mutationContext = new MutationContext();
 
   const visitor: ts.Visitor = (node: ts.Node): ts.Node => {
+    const parent = node.parent;
     node = onBeforeVisit(node, mutationContext, context);
 
-    if (DEBUG && !mutationContext.wasVisited(node)) {
+    if (DEBUG) {
       const scope = mutationContext.scope ? ts.SyntaxKind[mutationContext.scope.kind] : 'undefined';
+      console.log(`Visited: ${mutationContext.wasVisited(node) ? 'Yes' : 'No'}`)
       console.log(`Scope: ${scope}`);
+      console.log(`Kind:  ${ts.SyntaxKind[node.kind]} (${node.kind})`);
+      console.log(`File:  ${mutationContext.sourceFile.fileName}`);
     }
 
     for (let mutator of mutators) {
       node = mutator.mutateNode(node, mutationContext);
-      mutationContext.addVisited(node);
     }
 
-    if (DEBUG && node.parent) {
-      console.log(`Kind:  ${ts.SyntaxKind[node.kind]} (${node.kind})`);
-      console.log(`File:  ${mutationContext.sourceFile.fileName}`);
+    node.parent = parent;
+    util.setParent(node);
+
+    if (DEBUG) {
       console.log('<============================================');
-      console.log(node.getText());
+      if (node.parent) {
+        if (node.flags === ts.NodeFlags.Synthesized) {
+          console.log('Cannot get text of synthesized node.');
+        } else {
+          console.log(node.getText());
+        }
+      } else {
+        console.log('Parent not set, cannot get text.');
+      }
       console.log('============================================>');
+    }
+
+
+    if (DEBUG) {
       console.log('\n\n');
     }
 
@@ -191,7 +211,7 @@ function toTempFilePath(file: string, basePath: string, tempFolderName: string):
 
 function check(diagnostics: ts.Diagnostic[]): boolean {
   if (diagnostics && diagnostics.length > 0) {
-    bus.emitter.emit(bus.events.DIAGNOSTICS, { diagnostics });
+    notify(bus.events.DIAGNOSTICS, { diagnostics });
     console.error(ts.formatDiagnostics(diagnostics, {
       getCurrentDirectory: () => ts.sys.getCurrentDirectory(),
       getNewLine: () => ts.sys.newLine,
@@ -201,4 +221,8 @@ function check(diagnostics: ts.Diagnostic[]): boolean {
   }
 
   return true;
+}
+
+function notify(event: string | symbol, ...args: any[]): boolean {
+  return bus.emitter.emit(event, args);
 }
