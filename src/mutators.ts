cbdcbfde2dc7c756c767bb03e5f7a41625ba943d
Fabian Pirklbauer <fabian.pirklbauer@gmail.com>
2017-03-27 17:39:40 +0200

    Added mutator for type alias declarations.

--- src/mutators.ts
+++ src/mutators.ts
@@ -1,17 +1,20 @@
 import { Mutator } from './mutators/Mutator';
 import { BinaryExpressionMutator } from './mutators/BinaryExpressionMutator';
 import { InterfaceDeclarationMutator } from './mutators/InterfaceDeclararionMutator';
+import { TypeAliasDeclarationMutator } from './mutators/TypeAliasDeclararionMutator';
 import { VariableDeclarationListMutator } from './mutators/VariableDeclarationListMutator';
 
 export {
   Mutator,
   BinaryExpressionMutator,
   InterfaceDeclarationMutator,
+  TypeAliasDeclarationMutator,
   VariableDeclarationListMutator
 };
 
 export const mutators: Mutator[] = [
   new BinaryExpressionMutator(),
   new InterfaceDeclarationMutator(),
+  new TypeAliasDeclarationMutator(),
   new VariableDeclarationListMutator(),
 ];
