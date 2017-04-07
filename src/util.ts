48a47b8bf3020cc186071530d51dd1e0a54ace3b
Fabian Pirklbauer <fabian.pirklbauer@gmail.com>
2017-04-07 23:00:40 +0200

    Commented out unused functions in util.

--- src/util.ts
+++ src/util.ts
@@ -1,7 +1,5 @@
 import * as ts from 'typescript';
 
-export type Scope = ts.SourceFile | ts.Block | ts.ModuleBlock | ts.CaseBlock;
-
 export function asArray<T>(value: T | T[]): T[] {
   return Array.isArray(value) ? value : [value];
 }
@@ -14,49 +12,50 @@ export function setParent(node: ts.Node): void {
   });
 }
 
-export function getScope(node: ts.Node): ts.Node {
-  if (node.kind === ts.SyntaxKind.SourceFile) {
-    return node;
-  }
-
-  if (isScopeKind(node.kind)) {
-    return node;
-  }
-
-  return getScope(node.parent);
+export function isKind(node: ts.Node, ...kind: ts.SyntaxKind[]): boolean {
+  return kind.indexOf(node.kind) !== -1;
 }
 
-export function isAnyOrLiteralKind(kind: ts.SyntaxKind): boolean {
-  return kind === ts.SyntaxKind.AnyKeyword || kind === ts.SyntaxKind.LiteralType;
+export function isBindingName(node: ts.Node) {
+  return isKind(node, ts.SyntaxKind.Identifier, ts.SyntaxKind.ArrayBindingPattern, ts.SyntaxKind.ObjectBindingPattern);
 }
 
-// TODO: test
-export function identifierExistsUp(node: ts.Node, id: string | ts.Identifier) {
-  if (typeof id !== 'string') id = id.text;
-  let current = node;
-  while (current.parent) current = current.parent;
-  return identifierExistsDown(current, id, node);
-}
-
-// TODO: test
-export function identifierExistsDown(node: ts.Node, id: string, stop?: ts.Node): boolean {
-  if (!node || node === stop) return false;
-  return node.kind === ts.SyntaxKind.Identifier && (node as ts.Identifier).text === id ?
-    true : !!ts.forEachChild(node, n => identifierExistsDown(n, id, stop));
-}
-
-export function isScopeKind(kind: ts.SyntaxKind): boolean {
-  switch (kind) {
-    case ts.SyntaxKind.SourceFile:
-    case ts.SyntaxKind.Block:
-    case ts.SyntaxKind.ModuleBlock:
-    case ts.SyntaxKind.CaseBlock:
-      return true;
-    default:
-      return false;
-  }
-}
-
-export function isKind(node: ts.Node, kind: ts.SyntaxKind): boolean {
-  return node.kind === kind;
-}
+// export function getScope(node: ts.Node): ts.Node {
+//   if (node.kind === ts.SyntaxKind.SourceFile) {
+//     return node;
+//   }
+//
+//   if (isScopeKind(node.kind)) {
+//     return node;
+//   }
+//
+//   return getScope(node.parent);
+// }
+//
+//
+// // TODO: test
+// export function identifierExistsUp(node: ts.Node, id: string | ts.Identifier) {
+//   if (typeof id !== 'string') id = id.text;
+//   let current = node;
+//   while (current.parent) current = current.parent;
+//   return identifierExistsDown(current, id, node);
+// }
+//
+// // TODO: test
+// export function identifierExistsDown(node: ts.Node, id: string, stop?: ts.Node): boolean {
+//   if (!node || node === stop) return false;
+//   return node.kind === ts.SyntaxKind.Identifier && (node as ts.Identifier).text === id ?
+//     true : !!ts.forEachChild(node, n => identifierExistsDown(n, id, stop));
+// }
+//
+// export function isScopeKind(kind: ts.SyntaxKind): boolean {
+//   switch (kind) {
+//     case ts.SyntaxKind.SourceFile:
+//     case ts.SyntaxKind.Block:
+//     case ts.SyntaxKind.ModuleBlock:
+//     case ts.SyntaxKind.CaseBlock:
+//       return true;
+//     default:
+//       return false;
+//   }
+// }
