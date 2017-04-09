78ef8e1ab50c82ee2537bb466fc9fab0037ff0a3
Fabian Pirklbauer <fabian.pirklbauer@gmail.com>
2017-04-09 11:49:03 +0200

    Added missing mutators to list.

--- src/mutators.ts
+++ src/mutators.ts
@@ -1,22 +1,31 @@
 import { Mutator } from './mutators/Mutator';
+import { ArrowFunctionMutator } from './mutators/ArrowFunctionMutator';
 import { BinaryExpressionMutator } from './mutators/BinaryExpressionMutator';
+import { BlockLikeMutator } from './mutators/BlockLikeMutator';
 import { ClassDeclarationMutator } from './mutators/ClassDeclarationMutator'
+import { FunctionExpressionMutator } from './mutators/FunctionExpressionMutator';
 import { InterfaceDeclarationMutator } from './mutators/InterfaceDeclararionMutator';
 import { TypeAliasDeclarationMutator } from './mutators/TypeAliasDeclararionMutator';
 import { VariableDeclarationListMutator } from './mutators/VariableDeclarationListMutator';
 
 export {
   Mutator,
+  ArrowFunctionMutator,
   BinaryExpressionMutator,
+  BlockLikeMutator,
   ClassDeclarationMutator,
+  FunctionExpressionMutator,
   InterfaceDeclarationMutator,
   TypeAliasDeclarationMutator,
   VariableDeclarationListMutator,
 };
 
 export const mutators: Mutator[] = [
+  new ArrowFunctionMutator(),
   new BinaryExpressionMutator(),
+  new BlockLikeMutator(),
   new ClassDeclarationMutator(),
+  new FunctionExpressionMutator(),
   new InterfaceDeclarationMutator(),
   new TypeAliasDeclarationMutator(),
   new VariableDeclarationListMutator(),
