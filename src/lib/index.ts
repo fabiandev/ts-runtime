7774f1d7b97b1d2b55f93a225689a0976c5860c8
Fabian Pirklbauer <fabian.pirklbauer@gmail.com>
2017-06-28 22:56:37 +0200

    Unwrap intersection types.

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,33 +1,45 @@
 import * as t from 'flow-runtime';
 
 const voidType = t.void;
+const intersect = t.intersect;
+
+t.intersect = (...args: any[]) => {
+  return intersect.bind(t)(...args).unwrap();
+}
+
+t.intersection = (...args: any[]) => {
+  return t.intersect(...args);
+}
 
 t.undef = () => {
-  return voidType();
+  return voidType.bind(t)();
 }
 
 t.nostrict = (...args: any[]) => {
-  return t.union(...args, t.null())
+  return t.bind(t).union(...args, t.null.bind(t)())
 }
 
 t.void = () => {
-  return t.union(t.null(), t.undef());
+  return t.union.bind(t)(
+    t.null.bind(t)(),
+    t.undef.bind(t)()
+  );
 }
 
 t.n = (...args: any[]) => {
-  return t.nullable(...args);
+  return t.nullable.bind(t)(...args);
 }
 
 t.enum = (...args: any[]) => {
-  return t.union(...args);
+  return t.union.bind(t)(...args);
 }
 
 t.enumMember = (arg: any) => {
-  return t.number(arg);
+  return t.number.bind(t)(arg);
 }
 
 t.enumRef = (...args: any[]) => {
-  return t.typeOf(...args);
+  return t.typeOf.bind(t)(...args);
 }
 
 export default t;
