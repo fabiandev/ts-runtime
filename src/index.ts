b0ca9904fe317729d029305e60f24d25399e02d3
Fabian Pirklbauer <fabian.pirklbauer@gmail.com>
2017-08-02 19:50:20 +0200

    Only expose options, transform and bus in main.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,6 @@
 import * as bus from './bus';
-import * as util from './util';
 
-export { bus, util };
-export * from './context';
-export * from './factory';
-export * from './mutators';
 export * from './options';
-export * from './scanner';
 export * from './transform';
+
+export { bus };
