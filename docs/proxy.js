b65166cd1f0be2c862b48240d95b4c01c1037b09
Fabian Pirklbauer <fabian.pirklbauer@gmail.com>
2017-08-20 12:07:10 +0200

    Latest version build including Monaco Editor upgrade.

--- docs/proxy.js
+++ docs/proxy.js
@@ -1,5 +1,5 @@
 self.MonacoEnvironment = {
-  baseUrl: 'https://unpkg.com/monaco-editor@^0.9.0/min/'
+  baseUrl: 'https://unpkg.com/monaco-editor@^0.10.0/min/'
 };
 
-importScripts('https://unpkg.com/monaco-editor@^0.9.0/min/vs/base/worker/workerMain.js');
+importScripts('https://unpkg.com/monaco-editor@^0.10.0/min/vs/base/worker/workerMain.js');
