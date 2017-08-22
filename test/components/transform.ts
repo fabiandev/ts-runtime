727313d89c1ec0e5be7351d00a0405356cdbefe8
Fabian Pirklbauer <fabian.pirklbauer@gmail.com>
2017-08-22 02:05:45 +0200

    Added transform tests.

--- test/components/transform.ts
+++ test/components/transform.ts
@@ -5,5 +5,26 @@ export default () => {
       const result = util.transform(input);
       expect(result).ok();
     });
+
+    it('should enable the preserveConstEnums option', () => {
+      const input = `const enum Foo { }`;
+
+      const result = util.transform(input, { compilerOptions: { preserveConstEnums: false } });
+
+      const expected = `
+      import t from "ts-runtime/lib";
+
+      var Foo;
+      (function (Foo) {
+      })(Foo || (Foo = {}));
+      t.annotate(Foo, t.enum());`;
+
+      util.compare(result, expected);
+    });
+
+    it('should enable the experimentalDecorators option', () => {
+      const input = ``;
+      const result = util.transform(input, { compilerOptions: { experimentalDecorators: false } });
+    });
   });
 };
