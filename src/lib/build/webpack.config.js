fae01bd7de6d447d0c47c15128dc55fe2450800d
Fabian Pirklbauer <fabian.pirklbauer@gmail.com>
2017-08-23 11:14:27 +0200

    Added lib umd build.

---
+++ src/lib/build/webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const config = {};
+
+config.paths = {
+  src: '../',
+  dest: '../../../dist/lib'
+};
+
+config.typescript = {
+  configFileName: './tsconfig.json'
+};
+
+config.webpack = [
+  {
+    entry: {
+      'ts-runtime.lib': path.normalize(path.join(__dirname, config.paths.src, 'index.ts')),
+      'ts-runtime.lib.min': path.normalize(path.join(__dirname, config.paths.src, 'index.ts'))
+    },
+    output: {
+      filename: '[name].js',
+      path: path.normalize(path.join(__dirname, config.paths.dest)),
+      library: {
+        root: 'tsr',
+        amd: 'ts-runtime/lib',
+        commonjs: 'ts-runtime/lib'
+      },
+      libraryTarget: 'umd'
+    },
+    devtool: 'source-map',
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js']
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          use: ["source-map-loader"],
+          enforce: "pre"
+        }, {
+          test: /\.tsx?$/,
+          use: ["source-map-loader"],
+          enforce: "pre"
+        }, {
+          test: /\.tsx?$/,
+          exclude: /\.d\.ts$/,
+          loader: 'ts-loader',
+          options: config.typescript
+        }
+      ]
+    },
+    plugins: [new webpack.optimize.UglifyJsPlugin({include: /\.min\.js$/, sourceMap: true})]
+  }
+];
+
+module.exports = config.webpack;
