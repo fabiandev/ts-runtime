0039434fe4e9cbd7b65caadbc9a284b4555e07cb
Fabian Pirklbauer <fabian.pirklbauer@gmail.com>
2017-08-24 15:10:49 +0200

    Added setCompilerOption to playground global object.

--- src/docs/index.ts
+++ src/docs/index.ts
@@ -130,10 +130,19 @@ function ready(): void {
 function expose() {
   (window as any).tsp.options = defaultOptions;
   (window as any).tsp.compile = onCodeChange;
+  (window as any).tsp.emit = onCodeChange;
   (window as any).tsp.run = () => runCode();
+
   (window as any).tsp.sync = () => {
     initOptions();
     updateCompilerOptions();
+  };
+
+  (window as any).tsp.setCompilerOption = (name: string, value: any) => {
+    (window as any).tsp.compilerOptions[name] = value;
+    initOptions();
+    updateCompilerOptions();
+    onCodeChange();
   };
 }
 
